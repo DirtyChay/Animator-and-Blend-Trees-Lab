1e0a0e2b0b5b4a9401b154c58a9d4af09a19c6b7
Chayan Tronson <chayan995@gmail.com>
2022-09-13 22:13:16 -0700

    testing step

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    private Animator animator;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.A))) {
+            // run
+            animator.SetFloat("Speed", 1);
+            transform.position = (Vector2)transform.position + new Vector2(-5, 0) * Time.deltaTime;
+            sr.flipX = true;
+        }
+        else if ((Input.GetKey(KeyCode.RightArrow)) || (Input.GetKey(KeyCode.D))) {
+            animator.SetFloat("Speed", 1);
+            transform.position = (Vector2)transform.position + new Vector2(5, 0) * Time.deltaTime;
+            sr.flipX = false;
+        }
+        else {
+            animator.SetFloat("Speed", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            // jump
+            animator.SetTrigger("Jump");
+            rb.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+        }
+    }
+}
